948fea81f019725edfe8d44d8e6eb4ebd5c8da28
Muhammad Jamshaid Aziz <jamshaid.aziz121@gmail.com>
2025-09-01 01:46:39 +0500

    Apna-college

---
+++ Start/lec # 1/lec-1.py
@@ -0,0 +1,74 @@
+# make a calculator
+
+num1 = float(input("Enter a number  "))
+num2 = float(input("Enter another number  "))
+
+operators = input("Enter operator : +-*/")
+
+print(operators)
+
+if operators == "+":
+    result = num1 + num2
+    print("The sum is", result)
+    
+elif operators == "-":
+    result = num1 - num2
+    print("The subtraction is", result)
+
+elif operators == "*":
+    result = num1 * num2
+    print("The multiplication is", result)
+
+elif operators == "/":
+    result = num1 / num2
+    print("The division is", result)
+
+else:
+    print("Invalid operator")
+
+
+
+# Q - 1
+# take input from user and print it
+# Calculator
+
+# a = input ("Enter 1st number")
+# b = input("Enter 2nd number")
+# areaOfsqure = float(input("Enter side of squre"))
+
+# area = areaOfsqure * areaOfsqure
+
+# a = float(a)
+# b = float(b)
+
+# sum = a + b
+# sub = a - b
+# multi = a * b
+# div = a / b
+
+
+# print(sum)
+# print(sub)
+# print(multi)
+# print(div)
+# print("Area of squre is", area)
+
+ # start
+# firstName = " Jamshaid "
+
+# age = 42.6
+
+# rollNO = 25
+
+# print(type(firstName))
+# print(type(age))
+# print(type(rollNO  ))
+
+# Data type
+# data1 = False
+# data2 = None
+# print(type(data1))
+# print(type(data2))
+
+
+
