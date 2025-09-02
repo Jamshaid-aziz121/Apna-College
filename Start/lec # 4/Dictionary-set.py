52b504f48f5e24a98b951f17edac74f40019dfdb
Muhammad Jamshaid Aziz <jamshaid.aziz121@gmail.com>
2025-09-03 01:50:40 +0500

    lec # 4

---
+++ Start/lec # 4/Dictionary-set.py
@@ -0,0 +1,12 @@
+dict = {
+    "name" : "jamshaid",
+    "surName" : "aziz",
+    "subjects" : {
+        "math" : 98,
+        "electrnice" : 75,
+        "english": 80
+
+    }
+}
+
+print(dict["subjects"]["english"])
